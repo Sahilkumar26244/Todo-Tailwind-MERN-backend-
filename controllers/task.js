f97038cd107eab5370f5e0523aadab424a27ec37
Sahil Kumar <kumarsahil8981@gmail.com>
2025-01-22 15:57:20 +0530

    task delete function are done

--- controllers/task.js
+++ controllers/task.js
@@ -1,21 +1,50 @@
 const Task = require("../models/task");
-const User = require("../models/user")
+const User = require("../models/user");
 
-const createTask = async(req,res) => {
-    try {
-        const {title,desc} = req.body;
-        const {id} = req.headers;
-        const newTask = new Task({title:title,desc:desc})
-        const saveTask = await newTask.save()
-        const taskId = saveTask._id;
-        await User.findByIdAndUpdate(id, {$push:{tasks:taskId._id}})
-        res.status(200).json({message:"Task created"})
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({message:"Internal server error!"})
-    }
-}
+const createTask = async (req, res) => {
+  try {
+    const { title, desc } = req.body;
+    const { id } = req.headers;
+    const newTask = new Task({ title: title, desc: desc });
+    const saveTask = await newTask.save();
+    const taskId = saveTask._id;
+    await User.findByIdAndUpdate(id, { $push: { tasks: taskId._id } });
+    res.status(200).json({ message: "Task created" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Internal server error!" });
+  }
+};
+
+const getTasks = async (req, res) => {
+  try {
+    const { id } = req.headers;
+    const userData = await User.findById(id).populate({
+      path: "tasks",
+      options: { sort: { createdAt: -1 } },
+    });
+    res.status(200).json({ data: userData });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Internal server error!" });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.headers.id;
+    await Task.findByIdAndDelete(id);
+    await User.findByIdAndUpdate(userId, { $pull: {tasks:id} });
+    res.status(200).json({ message: "Task deleted!" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Internal server error!" });
+  }
+};
 
 module.exports = {
-    createTask
-}
+  createTask,
+  getTasks,
+  deleteTask,
+};
--- routes/task.js
+++ routes/task.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const {createTask} = require("../controllers/task")
+const {createTask, getTasks, deleteTask} = require("../controllers/task")
 const {authenticationToken} = require("./auth")
 
 const router = express.Router();
 
 router.route('/').post(authenticationToken,createTask);
-router.route('/login').post();
+router.route('/get').get(authenticationToken,getTasks);
+router.route('/delete/:id').delete(authenticationToken,deleteTask)
 
 
 module.exports = router
